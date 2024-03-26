95ef421eb6bb2bd98e7db500edd46b74cb64b2a4
Sandy! <sandydupham@gmail.com>
2024-03-25 21:47:16 -0400

    Initial commit
    we like spring initializr

---
+++ src/main/java/com/project/sandyswebpage/SandysWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.project.sandyswebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandysWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandysWebpageApplication.class, args);
+	}
+
+}
